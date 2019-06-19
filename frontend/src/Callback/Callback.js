04171f1d217d2a012a632d3f876cfeee4538cf85
Dave Kanter <moatgoat@hotmail.com>
2019-06-19 15:33:53 -0700

    GOT THE AUTH WORKING!!!!!!!

--- frontend/src/Callback/Callback.js
+++ frontend/src/Callback/Callback.js
@@ -2,16 +2,27 @@
 
 
 import React, { Component } from 'react'
-import loading from '../img/cake-pops.jpg'
+//import loading from '../img/cake-pops.jpg'
+import loading from './loading.svg'
 
 class Callback extends Component {
   render() {
-    const style = {display: inherit;} //...
+    const style = {
+      position: 'absolute',
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      width: '100vw',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'white',
+    }
 
     return (
       <div style={style}>
         <img src={loading} alt="loading"/>
-        This is just a test
       </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,11 @@ import './css/index.css'
 import './css/unsemantic-grid-responsive-tablet-no-ie7.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import { makeMainRoutes } from './routes'
+
+const routes = makeMainRoutes()
  
 ReactDOM.render(
-  <Main/>, document.getElementById("root")
+  //routes, <Main/>, document.getElementById("root")
+  routes, document.getElementById('root')
 );
