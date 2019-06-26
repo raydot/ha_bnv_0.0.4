7722436eeed9b53b899dde65df27d874e68e1e76
Dave Kanter <moatgoat@hotmail.com>
2019-06-26 15:05:03 -0700

    Delete oktaClient.js

--- server/lib/oktaClient.js
+++
@@ -1,8 +0,0 @@
-const okta = require('@okta/okta-sdk-nodejs')
-
-const client = new okta.Client({
-  orgUrl: 'https://dev-310767.okta.com',
-  token: '00WUv5Mhv_ScDMJ6WizO52NwwqVSkvQ-geWHhJAZg1'
-})
-
-module.exports = client
